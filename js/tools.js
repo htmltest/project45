22fdb02a1fb0c1466b264386bf4f3fc7473aba80
htmltest <struev.vs@gmail.com>
2015-10-08 13:06:01 +0300

    produkt info block

--- js/tools.js
+++ js/tools.js
@@ -34,6 +34,66 @@
             });
         });
 
+        $('.detail-photo-big a').fancybox({
+            tpl : {
+                closeBtn : '<a title="Закрыть" class="fancybox-item fancybox-close" href="javascript:;"></a>',
+                next     : '<a title="Следующая" class="fancybox-nav fancybox-next" href="javascript:;"><span></span></a>',
+                prev     : '<a title="Предыдущая" class="fancybox-nav fancybox-prev" href="javascript:;"><span></span></a>'
+            }
+        });
+
+        $('.detail-photo-preview a').click(function(e) {
+            var curLink = $(this);
+            var curLi = curLink.parent();
+
+            if (!curLi.hasClass('active')) {
+                $('.detail-photo-preview li.active').removeClass('active');
+                curLi.addClass('active');
+                $('.detail-photo-big a').attr('href', curLink.attr('rel'));
+                $('.detail-photo-big img').attr('src', curLink.attr('href'));
+            }
+
+            e.preventDefault();
+        });
+
+        $('.detail-color-item input:checked').parent().addClass('checked');
+        $('.detail-color-item').click(function() {
+            var curName = $(this).find('input').attr('name');
+            $('.detail-color-item input[name="' + curName + '"]').parent().removeClass('checked');
+            $(this).addClass('checked');
+            $(this).find('input').prop('checked', true).trigger('change');
+        });
+
+        $('.detail-count input').change(function() {
+            var curInput = $(this);
+            var curValue = Number(curInput.val());
+            if (!curValue || (curValue < 1)) {
+                curValue = 1;
+                curInput.val('1');
+            }
+        });
+
+        $('.detail-count-dec').click(function(e) {
+            var curInput = $(this).parent().find('input');
+            var curValue = Number(curInput.val());
+            curValue--;
+            if (curValue < 1) {
+                curValue = 1;
+            }
+            curInput.val(curValue);
+
+            e.preventDefault();
+        });
+
+        $('.detail-count-inc').click(function(e) {
+            var curInput = $(this).parent().find('input');
+            var curValue = Number(curInput.val());
+            curValue++;
+            curInput.val(curValue);
+
+            e.preventDefault();
+        });
+
     });
 
 })(jQuery);
